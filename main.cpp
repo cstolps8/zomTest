58918df3609e07140413a86ac98cae2a0bfd3ea4
cstolps8 <stolpacj08@uww.edu>
2016-10-10 22:25:17 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Collin Stolpa
+ * Description: 
+ * 
+ * Created on October 10, 2016, 10:21 PM
+ */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
+
